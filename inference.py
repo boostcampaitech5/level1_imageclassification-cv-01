410a053752c05ef58c4295ad9c443920ad341947
01km <dkdlel4990@gmail.com>
2023-04-26 02:48:13 +0000

    feat inference

--- inference.py
+++ inference.py
@@ -11,61 +11,16 @@ from dataset import TestDataset, MaskBaseDataset
 
 
 def load_model(saved_model, num_classes, device):
-    model_cls = getattr(import_module("model"), args.model)
-    model = model_cls(
-        num_classes=num_classes
-    )
-
-    # tarpath = os.path.join(saved_model, 'best.tar.gz')
-    # tar = tarfile.open(tarpath, 'r:gz')
-    # tar.extractall(path=saved_model)
-
-    model_path = os.path.join(saved_model, 'best.pth')
-    model.load_state_dict(torch.load(model_path, map_location=device))
-
+    # model_path = os.path.join(saved_model, 'best.pth')
+    # 가장 acc가 좋았던 결과를 가져와서 model 학습
     return model
 
 
 @torch.no_grad()
 def inference(data_dir, model_dir, output_dir, args):
     """
+    테스트 과정...
     """
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    num_classes = MaskBaseDataset.num_classes  # 18
-    model = load_model(model_dir, num_classes, device).to(device)
-    model.eval()
-
-    img_root = os.path.join(data_dir, 'images')
-    info_path = os.path.join(data_dir, 'info.csv')
-    info = pd.read_csv(info_path)
-
-    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
-    dataset = TestDataset(img_paths, args.resize)
-    loader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=args.batch_size,
-        num_workers=multiprocessing.cpu_count() // 2,
-        shuffle=False,
-        pin_memory=use_cuda,
-        drop_last=False,
-    )
-
-    print("Calculating inference results..")
-    preds = []
-    with torch.no_grad():
-        for idx, images in enumerate(loader):
-            images = images.to(device)
-            pred = model(images)
-            pred = pred.argmax(dim=-1)
-            preds.extend(pred.cpu().numpy())
-
-    info['ans'] = preds
-    save_path = os.path.join(output_dir, f'output.csv')
-    info.to_csv(save_path, index=False)
-    print(f"Inference Done! Inference result saved at {save_path}")
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
